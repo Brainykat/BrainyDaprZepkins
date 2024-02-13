d1f189dbe2e3225751c540dc7e61d8ba7bee80b3
Thuku Chris <eng.christhuku@gmail.com>
2024-02-13 16:12:24 +0300

    feat no idea

--- src/BuildingBlocks/HealthChecks/Healthchecks/GlobalUsings.cs
+++ src/BuildingBlocks/HealthChecks/Healthchecks/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System;
 global using Dapr.Client;
-global using Microsoft.eShopOnDapr.BuildingBlocks.Healthchecks;
 global using Microsoft.Extensions.Diagnostics.HealthChecks;
 global using System.Threading;
 global using System.Threading.Tasks;
